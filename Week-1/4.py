c6f4d4fae8a3bd1e4f415ca0673043e54a007e81
5andeepNambiar <79718442+5andeepNambiar@users.noreply.github.com>
2021-05-01 00:19:06 +0530

    Upload files

--- Week-1/4.py
+++ Week-1/4.py
@@ -1,30 +1,3 @@
-# def sec_low(stud):
-#     grades=[]
-#     for j in stud:
-#         grades.append(j[1])
-    # sort_grades=sorted(grades)
-    # seclow_grade = sort_grades[0]
-    # for grade in sort_grades:
-    #     if(grade!=seclow_grade):
-    #         seclow_grade=grades
-    #         break
-    # seclow_stud=[]
-    # for j in stud:
-    #     if j[1] == seclow_grade:
-    #         seclow_stud.append(j[0])
-    # for name in sorted(seclow_stud):
-    #     print(name)
-
-
-
-# stud = []
-# n=int(input())
-# for i in range(n):
-#     stud_data=[input(),float(input())]
-#     stud.append(stud_data)
-# sec_low(stud)
-
-
 n=int(input())
 stud=[]
 grades=[]
